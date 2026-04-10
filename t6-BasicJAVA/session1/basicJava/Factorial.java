842bdac6999b38f36e570ab28d60b9ffe8d1b85a
Rachit Shivhare <rachitshivhare@gmail.com>
2026-04-10 16:37:42 +0530

    Basic Java Commit - 3 Added Factorial code

---
+++ t6-BasicJAVA/session1/basicJava/Factorial.java
@@ -0,0 +1,24 @@
+// Implement a program to find the factorial of a given number.
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            long factorial = 1;
+            for (int i = 1; i <= number; i++) {
+                factorial *= i;
+            }
+            System.out.println("Factorial of " + number + " is: " + factorial);
+        }
+
+        sc.close();
+        
+    }
+}

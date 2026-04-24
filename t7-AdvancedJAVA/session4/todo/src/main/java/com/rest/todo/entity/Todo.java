3c8af1d6888fcbfe008cd642dca826d1705dc47f
Rachit Shivhare <rachitshivhare@gmail.com>
2026-04-24 10:22:21 +0530

    session4 commit - 1 Added entities and repository

---
+++ t7-AdvancedJAVA/session4/todo/src/main/java/com/rest/todo/entity/Todo.java
@@ -0,0 +1,31 @@
+package com.rest.todo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TodoStatus status;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
